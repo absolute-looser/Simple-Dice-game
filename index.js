8994d48658e31b45d200d84205fe08b2ab82649f
absolute-looser <akhileshsingh2314@gmail.com>
2021-05-18 10:01:01 +0530

    changed the javascript library to Jquery

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var randomVar1 = 1 + (Math.round(Math.random() * 10) % 6);
-var randomVar2 = 1 + (Math.round(Math.random() * 10) % 6);
-var d = "images/dice" + randomVar1 + ".png";
-var c = document.getElementById("img1").setAttribute("src", d);
-d = "images/dice" + randomVar2 + ".png";
-c = document.getElementById("img2").setAttribute("src", d);
-if (randomVar1 > randomVar2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins";
-} else if (randomVar2 > randomVar1) {
-  document.querySelector("h1").innerHTML = "Player 2 wins";
-} else {
-  document.querySelector("h1").innerHTML = "Fuck yourself it's a draw!";
+function change() {
+  var randomVar1 = 1 + (Math.round(Math.random() * 10) % 6);
+  var randomVar2 = 1 + (Math.round(Math.random() * 10) % 6);
+  var t = new Date();
+  var d = "images/dice" + randomVar1 + ".png";
+  $("#img1").attr("src", d);
+  d = "images/dice" + randomVar2 + ".png";
+  $("#img2").attr("src", d);
+  if (randomVar1 > randomVar2) {
+    $("h1").html("Player 1 wins");
+  } else if (randomVar2 > randomVar1) {
+    $("h1").html("Player 2 wins");
+  } else {
+    $("h1").html("Fuck yourself it's a draw!");
+  }
 }
